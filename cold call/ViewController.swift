99235546e7ab032a66b8a382d5d576b46fd49b77
lamia <lamia.fd123@gmail.com>
2021-12-05 15:09:55 +0300

    Initial Commit

---
+++ cold call/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cold call
+//
+//  Created by لمياء فالح الدوسري on 01/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
