ddbce78daf7e65be34b9db175bd18b8959411f6b
lamia <lamia.fd123@gmail.com>
2021-12-05 15:48:05 +0300

    v1

--- cold call/ViewController.swift
+++ cold call/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var count=0
+    @IBOutlet weak var name: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    let names=["sara","nora","lamia","tmara","maha","wala"]
+    
+    
+    @IBAction func callbtn(_ sender: Any) {
+        if count < names.count{
+            name.text = names[count]
+            count += 1
+            
+        }else{
+            count = 0
+        }
+        
+    }
+    
 
 }
 
